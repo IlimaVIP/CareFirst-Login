e621c298ba0e00c96722a799081ddf7e96e8e8ad
ilima.vip <matikhan89@Mati-PC.home>
2017-01-28 02:05:14 -0500

    new screenshots

--- CareFirst/src/test/java/login.java
+++ CareFirst/src/test/java/login.java
@@ -21,21 +21,26 @@ public class login {
 		
 		@Given("^I want to open FireFox and start application$")
 		@Test(priority=1)
-		public void i_want_to_open_FireFox_and_start_application(){
+		public void i_want_to_open_FireFox_and_start_application() throws IOException{
 			driver=new FirefoxDriver();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		    driver.get("https://individual.carefirst.com/individuals-families/home.page");
+		    TakesScreenshot ts=(TakesScreenshot)driver;
+			File source = ts.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFileToDirectory(source, new File("./Screenshots/carefirst.png"));
+		
 		}
 
 		@When("^I click on search button it should click$")
 		@Test(priority=2)
-		public void i_click_on_search_button_it_should_click() throws IOException {
+		public void i_click_on_search_button_it_should_click() throws IOException, InterruptedException {
 		    driver.findElement(By.xpath(".//*[@id='1442093000057']/div/div[3]/p/a")).click();
-		    
+		    Thread.sleep(3000);
 		    TakesScreenshot ts=(TakesScreenshot)driver;
 			File source = ts.getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFileToDirectory(source, new File("./Screenshots/carefirst.png"));
-		}
+			FileUtils.copyFileToDirectory(source, new File("./Screenshots/carefirstlog.png"));
+		
+		   }
 
 		@Then("^close the browser$")
 		@AfterMethod
